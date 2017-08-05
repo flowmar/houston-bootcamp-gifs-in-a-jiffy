424a11c4742e6609642c042136d8c961eed80e16
Flowmar <omar.imam@kineticvortex.com>
2017-08-05 00:19:45 -0500

    make stylistic changes and add user interface, fix js logic and make into functions

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,19 +1,94 @@
-var topics = ["vortex", "spiral", "illusion", "spin", "tripy", "art", "3D" ]
-  $('button').on("click", function(){
-      var x = $(this).data("animal");
-      var queryURL = "http://api.giphy.com/v1/gifs/search?q="+ x + "&api_key=dc6zaTOxFJmzC&limit=10";
-      $.ajax({url:queryURL,method:"GET"})
-      .done(function(response){
-      for(var i=0; i < response.data.length; i++){
-        var gifDiv = $('<div>');
-        var p = $('<p>').text("Rating: " + response.data[i].rating);
-        // set resultgif to an <img> tag
-        var resultgif = $('<img>');
-        // give the animalImage <img> tag a 'src' attribute which is equal to the url for the fixed_height version of the image
-        resultgif.attr('src',response.data[i].images.fixed_height.url);
-        gifDiv.append(p);
-        gifDiv.append(resultgif);
-        $('#gifarea').append(gifDiv);
-      }
-      })
-    })
+// Topics array for buttons
+var topics = [
+  "vortex",
+  "spiral",
+  "illusion",
+  "spin",
+  "trippy",
+  "art",
+  "3D",
+  "shiny",
+  "glow",
+  "neon",
+  "uv"
+]
+
+var newTopic = "";
+var x = "";
+
+function populate() {
+  // Clear out buttonArea
+  $('#buttonArea').empty();
+
+  // Loop through topics and create a button for each one
+  for (var i = 0; i < topics.length; i++) {
+    $('#buttonArea').append("<button class='btn btn-default topics' data-topic='" + topics[i] + "'>" + topics[i] + "</button>");
+  }
+}
+
+// When the search button is clicked...
+$('#submit')
+  .on('click', function (event) {
+
+    // Prevent the default action of clicking the button
+    event.preventDefault();
+
+    // Place the user input into a variable
+    newTopic = $('input#search').val();
+
+    // Add the new topic to the topics array
+    topics.push(newTopic);
+
+    // Create a button for the new topic
+    $('#buttonArea').append("<button class='btn btn-default topics' data-topic='" + newTopic + "'>" + newTopic + "</button>");
+
+    populate();
+    gifSearch();
+
+  });
+
+function gifSearch() {
+  // When a topic button is clicked
+  $('.topics')
+    .on('click', function (event) {
+
+      // Place the data attribute 'topic' into a variable
+      x = $(this).data('topic');
+
+      // Use the 'x' variable to search for the given topic
+      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+      // Make an AJAX request to the queryURLs
+      $
+        .ajax({url: queryURL, method: "GET"})
+        .done(function (response) {
+
+          // Clear out anything that is in the gifarea
+          $('#gifarea').empty();
+
+          // For every item returned in the search...
+          for (var i = 0; i < response.data.length; i++) {
+
+            // Create variables with elements to contain the incoming response
+            var gifDiv = $('<div>');
+            var p = $('<p>').text("Rating: " + response.data[i].rating);
+
+            // set resultgif to an <img> tag
+            var resultgif = $('<img>');
+
+            // give the <img> tag a 'src' attribute which is equal to the url
+            resultgif.attr('src', response.data[i].images.fixed_height.url);
+
+            // Append the resultgif and rating to the gifDiv
+            gifDiv.append(resultgif);
+            gifDiv.append(p);
+
+            // Append the gifDiv to the gifArea
+            $('#gifarea').append(gifDiv);
+          }
+        });
+    });
+}
+
+populate();
+gifSearch();
