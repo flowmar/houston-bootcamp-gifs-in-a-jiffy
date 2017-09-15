0202648fb8c0c4a524acb0c51f6a8d583b147072
Flowmar <omar.imam@kineticvortex.com>
2017-09-15 00:24:45 -0500

    add animation, style changes and allow all GIFs to populate in a horizontal row

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,35 +1,27 @@
 // Topics array for buttons
-var topics = [
-  "vortex",
-  "spiral",
-  "illusion",
-  "spin",
-  "trippy",
-  "art",
-  "3D",
-  "shiny",
-  "glow",
-  "neon",
-  "uv"
-]
+var topics = ['vortex', 'spiral', 'illusion', 'spin', 'trippy', 'art', '3D', 'shiny', 'glow', 'neon', 'uv'];
 
-var newTopic = "";
-var x = "";
+var newTopic = '';
+var x = '';
 
 function populate() {
-  // Clear out buttonArea
-  $('#buttonArea').empty();
+    // Clear out buttonArea
+    $('#buttonArea').empty();
 
-  // Loop through topics and create a button for each one
-  for (var i = 0; i < topics.length; i++) {
-    $('#buttonArea').append("<button class='btn btn-default topics' data-topic='" + topics[i] + "'>" + topics[i] + "</button>");
-  }
+    // Loop through topics and create a button for each one
+    for (var i = 0; i < topics.length; i++) {
+        $('#buttonArea').append(
+            "<button class='btn btn-default default-color-dark z-depth-2 topics animated zoomIn' data-topic='" +
+            topics[i] +
+            "'>" +
+            topics[i] +
+            '</button>'
+        );
+    }
 }
 
 // When the search button is clicked...
-$('#submit')
-  .on('click', function (event) {
-
+$('#submit').on('click', function(event) {
     // Prevent the default action of clicking the button
     event.preventDefault();
 
@@ -40,52 +32,47 @@ $('#submit')
     topics.push(newTopic);
 
     // Create a button for the new topic
-    $('#buttonArea').append("<button class='btn btn-default topics' data-topic='" + newTopic + "'>" + newTopic + "</button>");
+    $('#buttonArea').append(
+        "<button class='btn btn-default topics' data-topic='" + newTopic + "'>" + newTopic + '</button>'
+    );
 
     populate();
     gifSearch();
-
-  });
+});
 
 function gifSearch() {
-  // When a topic button is clicked
-  $('.topics')
-    .on('click', function (event) {
+    // When a topic button is clicked
+    $('.topics').on('click', function(event) {
+        // Place the data attribute 'topic' into a variable
+        x = $(this).data('topic');
 
-      // Place the data attribute 'topic' into a variable
-      x = $(this).data('topic');
+        // Use the 'x' variable to search for the given topic
+        var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + x + '&api_key=dc6zaTOxFJmzC&limit=10';
 
-      // Use the 'x' variable to search for the given topic
-      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=dc6zaTOxFJmzC&limit=10";
+        // Make an AJAX request to the queryURLs
+        $.ajax({ url: queryURL, method: 'GET' }).done(function(response) {
+            // Clear out anything that is in the gifarea
+            $('#gifarea').empty();
 
-      // Make an AJAX request to the queryURLs
-      $
-        .ajax({url: queryURL, method: "GET"})
-        .done(function (response) {
+            // For every item returned in the search...
+            for (var i = 0; i < response.data.length; i++) {
+                // Create variables with elements to contain the incoming response
+                var gifDiv = $('<div>');
+                var p = $('<p>').text('Rating: ' + response.data[i].rating);
 
-          // Clear out anything that is in the gifarea
-          $('#gifarea').empty();
+                // set resultgif to an <img> tag
+                var resultgif = $('<img class="img-fluid img-thumbnail animated fadeIn">');
 
-          // For every item returned in the search...
-          for (var i = 0; i < response.data.length; i++) {
+                // give the <img> tag a 'src' attribute which is equal to the url
+                resultgif.attr('src', response.data[i].images.fixed_height.url);
 
-            // Create variables with elements to contain the incoming response
-            var gifDiv = $('<div>');
-            var p = $('<p>').text("Rating: " + response.data[i].rating);
+                // Append the resultgif and rating to the gifDiv
+                gifDiv.append(resultgif);
+                gifDiv.append(p);
 
-            // set resultgif to an <img> tag
-            var resultgif = $('<img>');
-
-            // give the <img> tag a 'src' attribute which is equal to the url
-            resultgif.attr('src', response.data[i].images.fixed_height.url);
-
-            // Append the resultgif and rating to the gifDiv
-            gifDiv.append(resultgif);
-            gifDiv.append(p);
-
-            // Append the gifDiv to the gifArea
-            $('#gifarea').append(gifDiv);
-          }
+                // Append the gifDiv to the gifArea
+                $('#gifarea').append(gifDiv);
+            }
         });
     });
 }
